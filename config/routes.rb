ddedf56d63cf9979e8fefc5997016d6d30239ff1
Alice Loudon <alice_loudon@icloud.com>
2017-05-11 09:11:07 +0100

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  scope path: 'api' do
-    resources :shows
-  end
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+  resources :users
+
+  get 'shows' => 'shows#index'
+  get 'users' => 'users#index'
 
 end
